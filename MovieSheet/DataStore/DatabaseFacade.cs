d18ad4517e5e68fb7c5125422b49f622a20613f1
dance2die <dontbothermewithspam@gmail.com>
2015-03-03 21:34:14 -0500

    Parsed sample texts

---
+++ MovieSheet/DataStore/DatabaseFacade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DataStore
+{
+	public static class DatabaseFacade
+	{
+		public static void Write(Movie movie)
+		{
+			using (var documentSession = RavenDbConfig.Store.OpenSession())
+			{
+				documentSession.Store(movie);
+				documentSession.SaveChanges();
+			}
+		}
+
+		public static void WriteRange(IEnumerable<Movie> movies)
+		{
+			using (var documentSession = RavenDbConfig.Store.OpenSession())
+			{
+				foreach (var movie in movies)
+				{
+					documentSession.Store(movie);
+				}
+				documentSession.SaveChanges();
+			}
+		}
+	}
+}
---
+++ MovieSheet/DataStore/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStore
+{
+	public class Movie
+	{
+		private System.Text.RegularExpressions.Group title;
+
+		public string Title { get; set; }
+		public string Line { get; set; }
+
+		public Movie(string title, string line)
+		{
+			Title = title;
+			Line = line;
+		}
+	}
+}
